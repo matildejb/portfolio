71f0fa10336ccc64813c0259fc91702ad2ce5bc5
matildejb <matildejimenezbedmar@gmail.com>
2024-06-11 12:47:45 +0200

    Ruta principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
     //Ruta principal, siempre que carge la app nos redirige a la home
-  { path: '', pathMatch: 'full', redirectTo: 'matilde' },
+  { path: '', pathMatch: 'full', redirectTo: '/matilde' },
   { path: 'matilde', component: ThreeSceneComponent },
   { path: 'about-me', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
